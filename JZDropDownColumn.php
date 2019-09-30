c41e05a425e497fbfec2641965569291b49fc4c8
Jay-69 <43809209+Jay-69@users.noreply.github.com>
2019-09-30 08:38:00 +0300

    Update JZDropDownColumn.php

--- JZDropDownColumn.php
+++ JZDropDownColumn.php
@@ -51,10 +51,10 @@ class JZDropDownColumn extends DataColumn{
                 if(is_array($key)){$key = json_encode($key);}
 		$this->format='raw';
 		$uniqId = uniqid();
-		if(isset($val) && $val!=null){
-                    $clickMe='';
+		if(isset($val) && $val != null){
+                    $clickMe = '';
 		} else {
-                    $clickMe='<span id="jzdc_empty_id_'.$uniqId.'" class="'.$this->emptyCssClass.'" onclick="jzDropDownColumnEdit(\''.$uniqId.'\',\''.$this->className.'\',\''.$this->attribute.'\',\''.Html::encode($key).'\',\''.$this->editUrl.'\',\''.$this->loaderPath.'\','.json_encode($this->updateAll).');" onmouseover="$(this).css(\'text-decoration\',\'underline\');" onmouseout="$(this).css(\'text-decoration\',\'none\');" style="cursor:pointer;">'.$this->emptyLabel.'</span>';
+                    $clickMe = '<span id="jzdc_empty_id_'.$uniqId.'" class="'.$this->emptyCssClass.'" onclick="jzDropDownColumnEdit(\''.$uniqId.'\',\''.$this->className.'\',\''.$this->attribute.'\',\''.Html::encode($key).'\',\''.$this->editUrl.'\',\''.$this->loaderPath.'\','.json_encode($this->updateAll).');" onmouseover="$(this).css(\'text-decoration\',\'underline\');" onmouseout="$(this).css(\'text-decoration\',\'none\');" style="cursor:pointer;">'.$this->emptyLabel.'</span>';
                 }				
                 return '<span id="jzdc_title_id_'.$uniqId.'" onclick="jzDropDownColumnEdit(\''.$uniqId.'\',\''.$this->className.'\',\''.$this->attribute.'\',\''.Html::encode($key).'\',\''.$this->editUrl.'\',\''.$this->loaderPath.'\','.json_encode($this->updateAll).');" onmouseover="$(this).css(\'text-decoration\',\'underline\');" onmouseout="$(this).css(\'text-decoration\',\'none\');" style="cursor:pointer;">'.$val.'</span>'.$clickMe.' <span id="jzdc_info_id_'.$uniqId.'" class="'.$this->infoCssClass.'"></span>'.Html::dropDownList('jzdc_input_'.$uniqId, ArrayHelper::getValue($this->list, ArrayHelper::getValue($model, $this->attribute)), $this->list,['id'=>'jzdc_input_id_'.$uniqId, 'class'=>$this->editCssClass, 'style'=>'display:none;', 'onChange' => 'jzDropDownColumnSave(\''.$uniqId.'\',\''.$this->className.'\',\''.$this->attribute.'\',\''.$key.'\',\''.$this->editUrl.'\',\''.$this->loaderPath.'\','.json_encode($this->updateAll).')']).'&nbsp;&nbsp;&nbsp;&nbsp;<span id="jzdc_deny_id_'.$uniqId.'" class="'.$this->denyCssClass.'" style="display:none;" onclick="jzDropDownColumnDeny(\''.$uniqId.'\');">'.$this->denyLabel.'</span>';
             } else {
